731c409519501ab90482f2915cbd6242abd30c25
shidder <d.shidder@gmail.com>
2020-06-29 00:00:22 +0300

    добавляет изменение уровня насыщенности

--- js/main.js
+++ js/main.js
@@ -161,36 +161,46 @@ var marvinEffect = document.querySelector('#effect-marvin');
 var phobosEffect = document.querySelector('#effect-phobos');
 var heatEffect = document.querySelector('#effect-heat');
 var noneEffect = document.querySelector('#effect-none');
+var effectLevel = document.querySelector('.effect-level');
+
 
 noneEffect.addEventListener('click', function () {
   imagePreview.removeAttribute('class');
   imagePreview.classList.add('effects__preview--none');
+  imagePreview.style.filter = 'none';
 });
 
 chromeEffect.addEventListener('click', function () {
   imagePreview.removeAttribute('class');
   imagePreview.classList.add('effects__preview--chrome');
+  imagePreview.style.filter = 'grayscale(1)';
 });
 
 sepiaEffect.addEventListener('click', function () {
   imagePreview.removeAttribute('class');
   imagePreview.classList.add('effects__preview--sepia');
+  imagePreview.style.filter = 'sepia(1)';
 });
 
 marvinEffect.addEventListener('click', function () {
   imagePreview.removeAttribute('class');
   imagePreview.classList.add('effects__preview--marvin');
+  imagePreview.style.filter = 'invert(100%)';
 });
 
 phobosEffect.addEventListener('click', function () {
   imagePreview.removeAttribute('class');
   imagePreview.classList.add('effects__preview--phobos');
+  imagePreview.style.filter = 'blur(3px)';
 });
 
 heatEffect.addEventListener('click', function () {
   imagePreview.removeAttribute('class');
   imagePreview.classList.add('effects__preview--heat');
+  imagePreview.style.filter = 'brightness(3)';
 });
+
+
 
 /*
 ==========================================================================================================
@@ -238,3 +248,71 @@ var validateHashTags = function () {
 };
 
 validateHashTags();
+
+/*
+==========================================================================================================
+ Изменение уровня насыщенности
+ */
+
+var effectPin = document.querySelector('.effect-level__pin');
+var effectLevelLine = document.querySelector('.effect-level__line');
+
+var effectLevelInput = document.querySelector('.effect-level__value');
+var effectLevelDepth = document.querySelector('.effect-level__depth');
+effectPin.style.left = '100' + '%';
+effectLevelDepth.style.width = '100' + '%';
+var  effectLevelLineWidth = 453;
+
+effectPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y
+    };
+    startCoords = {
+      x: moveEvt.clientX
+    };
+    effectPin.style.left = (effectPin.offsetLeft - shift.x) + 'px';
+    effectLevelDepth.style.width = (effectPin.offsetLeft - shift.x) + 'px';
+    if ((effectPin.offsetLeft - shift.x) < 0) {
+      effectPin.style.left = '0';
+      effectLevelDepth.style.width = '0';
+    }
+    if ((effectPin.offsetLeft - shift.x) > effectLevelLineWidth) {
+      effectPin.style.left = effectLevelLineWidth + 'px';
+      effectLevelDepth.style.width = effectLevelLineWidth + 'px';
+    }
+    effectLevelInput.value = ((effectPin.offsetLeft - shift.x) / effectLevelLineWidth) * 100;
+    if (imagePreview.classList.contains('effects__preview--chrome')) {
+      imagePreview.style.filter = 'grayscale(' + effectLevelInput.value / 100 + ')';
+    }
+    if (imagePreview.classList.contains('effects__preview--sepia')) {
+      imagePreview.style.filter = 'sepia(' + effectLevelInput.value / 100 + ')';
+    }
+    if (imagePreview.classList.contains('effects__preview--marvin')) {
+      imagePreview.style.filter = 'invert(' + effectLevelInput.value + '%)';
+    }
+    if (imagePreview.classList.contains('effects__preview--phobos')) {
+      imagePreview.style.filter = 'blur(' + effectLevelInput.value / 100 * 3 + 'px)';
+    }
+    if (imagePreview.classList.contains('effects__preview--heat')) {
+      imagePreview.style.filter = 'brightness(' + effectLevelInput.value / 100 * 3 + ')';
+    }
+
+  };
+   var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+
