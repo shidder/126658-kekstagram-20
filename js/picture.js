5dfdfcd007f91cbbe9e09c50b99725fd988b6d69
shidder <d.shidder@gmail.com>
2020-07-05 15:22:50 +0300

    убирает лишние пробелы после проверки travis

--- js/picture.js
+++ js/picture.js
@@ -1,24 +1,29 @@
 'use strict';
 
 (function () {
-  var renderPhotos = function (photos) {
+
+  var photosContainer = document.querySelector('.pictures');
+  var fragment = document.createDocumentFragment();
+  var renderPhoto = function (photo) {
     var photoTemplate = document.querySelector('#picture')
       .content
       .querySelector('.picture');
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < 25; i++) {
-      var photo = photos[i];
-      var photoElement = photoTemplate.cloneNode(true);
-      photoElement.querySelector('.picture__img').src = photo.url;
-      photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-      photoElement.querySelector('.picture__likes').textContent = photo.likes;
-      fragment.appendChild(photoElement);
+    var photoElement = photoTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = photo.url;
+    photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    photoElement.querySelector('.picture__likes').textContent = photo.likes;
+    return photoElement;
+  };
+
+  var renderPhotos = function (photos) {
+
+    for (var i = 0; i < photos.length; i++) {
+      fragment.appendChild(renderPhoto(photos[i]));
     }
-    var photosContainer = document.querySelector('.pictures');
     photosContainer.appendChild(fragment);
   };
 
-  window.picture = {
-    renderPhotos: renderPhotos
+   window.picture = {
+    renderPhotos: renderPhotos,
   };
 })();
